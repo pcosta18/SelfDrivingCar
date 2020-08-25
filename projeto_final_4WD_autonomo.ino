b2fafc96103d243667d41bcc04f3e21ee8da2808
Pedro Soares <58630295+pcosta18@users.noreply.github.com>
2020-08-25 16:18:01 +0100

    Add files via upload

--- projeto_final_4WD_autonomo.ino
+++ projeto_final_4WD_autonomo.ino
@@ -5,7 +5,7 @@
 #define trigger A0
 #define echo A1
 
-HCSR04 hc(trigger,echo);
+HCSR04 hc(trigger,echo);   //HCSR04 (trig pin , echo pin)
 Servo myservo;
 
 AF_DCMotor motordireito1(1);
@@ -18,19 +18,22 @@ float distanciaE;
 float distanciaD;
 
 void setup() {
-  // começar paraado
+
+// começar paraado
 motoresquerdo1.run(RELEASE);
 motordireito1.run(RELEASE);
 motordireito2.run(RELEASE);
 motoresquerdo2.run(RELEASE);
-// velocidade dos motores, convém não colocar a velocidade máxima para não gastar as baterias tão rapidamente
-motoresquerdo1.setSpeed(180);
-motordireito1.setSpeed(180);
-motordireito2.setSpeed(180);
-motoresquerdo2.setSpeed(180);
+
+// velocidade dos motores, convémnão colocar a velocidade máxima para não gastar as baterias tão rapidamente e não aquecer o arduino e evitar perdas de performance
+motoresquerdo1.setSpeed(130);
+motordireito1.setSpeed(130);
+motordireito2.setSpeed(130);
+motoresquerdo2.setSpeed(130);
+
 // servo na minha posição inicial. Pode variar de projeto para projeto
-myservo.attach(10);
-myservo.write(90);
+myservo.attach(10); // 10 é o pin ao qual corresponde SER1 no motor shield
+myservo.write(90); // 90º é o servo a olhar para a frente na nossa montagem
 delay(1000);
 
 
@@ -44,33 +47,47 @@ motordireito1.run(FORWARD);
 motordireito2.run(FORWARD);
 motoresquerdo2.run(FORWARD);
 
-
   // se a distancia for menor que 30, anda um bocado para tras, olha para a direita e para a esquerda , e escolhe a porxima direção
- if (distancia<30) {
+ while (distancia<30) {
+  
+  // parar e recuar um pouco
   parar();
   andar_atras();
   parar();
-  myservo.write(10); // olhar para a direita
+
+  // olhar para a direita e para a esquerda
+  // Nota: é importante colocar um delay depois de deslocar o servo e só depois ler a distância para que quando for lida, não esteja a ser lida quando o servo está em movimento
+  
+  // olhar para a direita
+  myservo.write(0); 
+  delay(1000);
   distanciaD=hc.dist();
+  
+  // olhar para a esquerda
+  myservo.write(180); 
   delay(1000);
-  myservo.write(170); // olhar para a esquerda
   distanciaE=hc.dist();
-  delay(1000);
-  myservo.write(90); //posicao inicial
+  
+  //posicao inicial
+  myservo.write(90);
   delay(500);
-  if (distanciaE>distanciaD){ // se a a distancia da esquerda for maior, vai para a esquerda
+  
+  if (distanciaE>distanciaD){ // se a a distancia da esquerda for maior, vai para a esquerda)
     virar_esquerda();
     distanciaD=0;
     distanciaE=0;
-  }
-  if (distanciaD>distanciaE){ // se a distancia da esquerda for maior, vai para a direita
+    delay(100);
+    }
+  else { // se a distancia da esquerda for maior, vai para a direita)
     virar_direita();
     distanciaD=0;
     distanciaE=0;
+    delay(100);
   }
-  
+  distancia = 400; // para sair do while
  }
 }
+
 
 void parar(){
   motoresquerdo1.run(RELEASE);
@@ -85,7 +102,7 @@ void andar_atras(){
   motordireito1.run(BACKWARD);
   motordireito2.run(BACKWARD);
   motoresquerdo2.run(BACKWARD);
-  delay(400);
+  delay(200);
 }
 
 void virar_esquerda(){
@@ -93,7 +110,7 @@ void virar_esquerda(){
   motordireito1.run(FORWARD);
   motordireito2.run(FORWARD);
   motoresquerdo2.run(BACKWARD);
-  delay(600);
+  delay(350);
 }
 
 void virar_direita(){
@@ -101,5 +118,5 @@ void virar_direita(){
   motordireito1.run(BACKWARD);
   motordireito2.run(BACKWARD);
   motoresquerdo2.run(FORWARD);
-  delay(600);
+  delay(350);
 }
